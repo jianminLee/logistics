c3b53837a38a59a76bae883942441350b1e50890
lijianmin <lijianmin520@gmail.com>
2019-08-22 11:25:20 +0800

    fix bug

--- src/Factory.php
+++ src/Factory.php
@@ -8,5 +8,20 @@ use Wythe\Logistics\Channel\Channel;
 
 class Factory extends \Wythe\Logistics\Factory
 {
+    /**
+     * 格式化类的名称.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function formatClassName(string $name): string
+    {
+        if (class_exists($name)) {
+            return $name;
+        }
+        $name = ucfirst(str_replace(['-', '_', ' '], '', $name));
 
+        return __NAMESPACE__."\\Channel\\{$name}Channel";
+    }
 }
